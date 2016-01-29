e1809cc0cff85448411a8995ef18e1a92434e18b
Andrin Bürli <andrinbuerli@bbv.ch>
2016-01-29 14:00:27 +0100

    resolving

---
+++ DriveManager/DriveManager.Core.Test/DriveAccountTest.cs
@@ -0,0 +1,29 @@
+﻿namespace DriveManager.Core.Test
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DriveAccountTest
+    {
+        private DriveAccount testee;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.testee = new DriveAccount();
+        }
+
+        [Test]
+        public void WhenAuthentication_ThenDownloadOfFilesIsPossible()
+        {
+            // Arrange -
+
+            // Act
+            this.testee.Authenticate();
+
+            // Assert
+            this.testee.IsAuthenticated.Should().BeTrue();
+        } 
+    }
+}
---
+++ DriveManager/DriveManager.Core/DriveAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace DriveManager.Core
+{
+    public class DriveAccount
+    {
+        public DriveAccount()
+        {
+            this.IsAuthenticated = false;
+        }
+
+        public void Authenticate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsAuthenticated { get; private set; }
+    }
+}
